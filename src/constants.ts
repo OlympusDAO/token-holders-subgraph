29c48d3e87b7601ef67b402385a37bc5f62ffa78
Jem <0x0xjem@gmail.com>
2022-11-10 12:44:48 +0400

    Re-org

---
+++ src/constants.ts
@@ -0,0 +1,49 @@
+export const ERC20_GOHM = "0x0ab87046fbb341d058f17cbc4c1133f25a20a52f";
+export const ERC20_OHM_V1 = "0x383518188c0c6d7730d91b2c03a03c837814a899";
+export const ERC20_SOHM_V1 = "0x04F2694C8fcee23e8Fd0dfEA1d4f5Bb8c352111F";
+export const ERC20_OHM_V2 = "0x64aa3364f17a4d01c6f1751fd97c2bd3d7e7f1d5";
+export const ERC20_SOHM_V2 = "0x04906695D6D12CF5459975d7C3C03356E4Ccd460";
+
+const NULL = "0x0000000000000000000000000000000000000000";
+export const IGNORED_ADDRESSES = [
+    ERC20_GOHM,
+    ERC20_OHM_V1,
+    ERC20_OHM_V2,
+    ERC20_SOHM_V1,
+    ERC20_SOHM_V2,
+    NULL,
+];
+
+export const TYPE_TRANSFER = "TRANSFER";
+export const TYPE_MINT = "MINT";
+export const TYPE_BURN = "BURN";
+
+const TOKENS = new Map<string, string>();
+TOKENS.set(ERC20_GOHM.toLowerCase(), "gOHM");
+TOKENS.set(ERC20_OHM_V1.toLowerCase(), "OHM V1");
+TOKENS.set(ERC20_SOHM_V1.toLowerCase(), "sOHM V1");
+TOKENS.set(ERC20_OHM_V2.toLowerCase(), "OHM V2");
+TOKENS.set(ERC20_SOHM_V2.toLowerCase(), "sOHM V2");
+
+export function getTokenName(address: string): string {
+    if (!TOKENS.has(address.toLowerCase())) {
+        return "";
+    }
+
+    return TOKENS.get(address.toLowerCase());
+}
+
+const TOKEN_DECIMALS = new Map<string, string>();
+TOKEN_DECIMALS.set(ERC20_GOHM.toLowerCase(), "18");
+TOKEN_DECIMALS.set(ERC20_OHM_V1.toLowerCase(), "9");
+TOKEN_DECIMALS.set(ERC20_SOHM_V1.toLowerCase(), "9");
+TOKEN_DECIMALS.set(ERC20_OHM_V2.toLowerCase(), "9");
+TOKEN_DECIMALS.set(ERC20_SOHM_V2.toLowerCase(), "9");
+
+export function getTokenDecimals(address: string): number {
+    if (!TOKEN_DECIMALS.has(address.toLowerCase())) {
+        return -1;
+    }
+
+    return parseInt(TOKEN_DECIMALS.get(address.toLowerCase()));
+}
--- src/handleEvent.ts
+++ src/handleEvent.ts
@@ -1,62 +1,13 @@
 import { Address, BigDecimal, BigInt, Bytes, log } from "@graphprotocol/graph-ts";
 
-import { BurnCall, MintCall, Transfer } from "../generated/gOHM/gOHM";
+import { Transfer } from "../generated/gOHM/gOHM";
 import { Token, TokenHolder, TokenHolderTransaction } from "../generated/schema";
 import { arrayIncludesLoose } from "./arrayHelper";
+import { IGNORED_ADDRESSES, getTokenDecimals, getTokenName, TYPE_TRANSFER } from "./constants";
 import { getISO8601StringFromTimestamp } from "./dateHelper";
 import { toDecimal } from "./decimalHelper";
 
 // Inspired by: https://github.com/xdaichain/token-holders-subgraph/blob/master/src/mapping.ts
-
-const ERC20_GOHM = "0x0ab87046fbb341d058f17cbc4c1133f25a20a52f";
-const ERC20_OHM_V1 = "0x383518188c0c6d7730d91b2c03a03c837814a899";
-const ERC20_SOHM_V1 = "0x04F2694C8fcee23e8Fd0dfEA1d4f5Bb8c352111F";
-const ERC20_OHM_V2 = "0x64aa3364f17a4d01c6f1751fd97c2bd3d7e7f1d5";
-const ERC20_SOHM_V2 = "0x04906695D6D12CF5459975d7C3C03356E4Ccd460";
-
-const NULL = "0x0000000000000000000000000000000000000000";
-const IGNORED_ADDRESSES = [
-  ERC20_GOHM,
-  ERC20_OHM_V1,
-  ERC20_OHM_V2,
-  ERC20_SOHM_V1,
-  ERC20_SOHM_V2,
-  NULL,
-];
-
-const TYPE_TRANSFER = "TRANSFER";
-const TYPE_MINT = "MINT";
-const TYPE_BURN = "BURN";
-
-const TOKENS = new Map<string, string>();
-TOKENS.set(ERC20_GOHM.toLowerCase(), "gOHM");
-TOKENS.set(ERC20_OHM_V1.toLowerCase(), "OHM V1");
-TOKENS.set(ERC20_SOHM_V1.toLowerCase(), "sOHM V1");
-TOKENS.set(ERC20_OHM_V2.toLowerCase(), "OHM V2");
-TOKENS.set(ERC20_SOHM_V2.toLowerCase(), "sOHM V2");
-
-function getTokenName(address: string): string {
-  if (!TOKENS.has(address.toLowerCase())) {
-    return "";
-  }
-
-  return TOKENS.get(address.toLowerCase());
-}
-
-const TOKEN_DECIMALS = new Map<string, string>();
-TOKEN_DECIMALS.set(ERC20_GOHM.toLowerCase(), "18");
-TOKEN_DECIMALS.set(ERC20_OHM_V1.toLowerCase(), "9");
-TOKEN_DECIMALS.set(ERC20_SOHM_V1.toLowerCase(), "9");
-TOKEN_DECIMALS.set(ERC20_OHM_V2.toLowerCase(), "9");
-TOKEN_DECIMALS.set(ERC20_SOHM_V2.toLowerCase(), "9");
-
-function getTokenDecimals(address: string): number {
-  if (!TOKEN_DECIMALS.has(address.toLowerCase())) {
-    return -1;
-  }
-
-  return parseInt(TOKEN_DECIMALS.get(address.toLowerCase()));
-}
 
 function createOrLoadToken(address: Address, name: string, blockchain: string): Token {
   const tokenId = `${name}/${blockchain}`;
@@ -101,9 +52,8 @@ function createTokenHolderTransaction(
   transactionLogIndex: BigInt,
 ): TokenHolderTransaction {
   // The balanceId incorporates the transactionLogIndex, so that multiple transfers within a single transaction can be recorded
-  const balanceId = `${
-    tokenHolder.id
-  }/${transaction.toHexString()}/${transactionLogIndex.toString()}`;
+  const balanceId = `${tokenHolder.id
+    }/${transaction.toHexString()}/${transactionLogIndex.toString()}`;
   const tokenBalance = new TokenHolderTransaction(balanceId);
   tokenBalance.balance = balance;
   tokenBalance.block = block;
@@ -214,33 +164,5 @@ export function handleTransfer(event: Transfer): void {
     event.transaction.hash,
     TYPE_TRANSFER,
     event.transactionLogIndex,
-  );
-}
-
-export function handleMint(call: MintCall): void {
-  updateTokenBalance(
-    call.to,
-    call.inputs._to,
-    call.inputs._amount,
-    false,
-    call.block.number,
-    call.block.timestamp,
-    call.transaction.hash,
-    TYPE_MINT,
-    BigInt.fromString("0"),
-  );
-}
-
-export function handleBurn(call: BurnCall): void {
-  updateTokenBalance(
-    call.to,
-    call.inputs._from,
-    call.inputs._amount,
-    true,
-    call.block.number,
-    call.block.timestamp,
-    call.transaction.hash,
-    TYPE_BURN,
-    BigInt.fromString("0"),
   );
 }
